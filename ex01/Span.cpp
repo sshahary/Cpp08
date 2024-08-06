4aa184fd209593e6c4c85cecdc732ab621c3a9de
sshahary <145033155+sshahary@users.noreply.github.com>
2024-08-06 17:31:42 +0200

    Span init

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 05:11:57 by sshahary          #+#    #+#             */
-/*   Updated: 2024/08/06 05:37:28 by sshahary         ###   ########.fr       */
+/*   Updated: 2024/08/06 17:30:40 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,41 @@ void Span::addNumber(int number)
 	numbers.push_back(number);
 }
 
+int Span::shortestSpan() const
+{
+	if (numbers.size() < 2)
+		throw NoSpanFoundException();
+
+	std::vector<int> sortedNumbers(numbers);
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+	int shortest = std::numeric_limits<int>::max();
+	for (std::vector<int>::iterator it = sortedNumbers.begin(); it != sortedNumbers.end() - 1; ++it)
+	{
+		int span = *(it + 1) - *it;
+		if (span < shortest)
+			shortest = span;
+	}
+	return (shortest);
+}
+
+int Span::longestSpan() const
+{
+	if (numbers.size() < 2)
+		throw NoSpanFoundException();
+
+	int minElement = *std::min_element(numbers.begin(), numbers.end());
+	int maxElement = *std::max_element(numbers.begin(), numbers.end());
+
+	return maxElement - minElement;
+}
+
+const char* Span::SpanFullException::what() const throw()
+{
+	return "Span is full";
+}
+
+const char* Span::NoSpanFoundException::what() const throw()
+{
+	return "No span found";
+}
